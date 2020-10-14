2ad287d7c49764dcd75590e7c257a06b80cb3b6c
SQuinn44 <65035946+SQuinn44@users.noreply.github.com>
2020-10-13 19:41:09 -0500

    update const

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
-const Manager = require("./lib/Manager");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+const util= require("util");
+
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+
+const mkdirAsync = util.promisify(fs.mkdir);
+const writeFileAsync = util.promisify(fs.writeFile);
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
+const Employee = require("./lib/htmlRenderer");
 const render = require("./lib/htmlRenderer");
 
+//Collect information and create objects
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
